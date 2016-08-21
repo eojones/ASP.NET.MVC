03e6c01201eb3406d297dc39bc0e6846fa0c47be
João Ferreira <joaov11@hotmail.com>
2016-08-21 18:34:57 -0300

    Cadastro de clientes (lista)

--- Exercicio01.MVC.WEB/Controllers/HomeController.cs
+++ Exercicio01.MVC.WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercicio01.MVC.WEB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,20 @@ using System.Web.Mvc;
 
 namespace Exercicio01.MVC.WEB.Controllers
 {
+   
     public class HomeController : Controller
     {
-        // GET: Home
+        private static List<Cliente> _listaClientes = new List<Cliente>();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AddToList(Cliente cli)
+        {
+            _listaClientes.Add(cli);
+            TempData["msg"] = "Cliente Adicionado!";
+            return Redirect("Index");
         }
     }
 }
---
+++ Exercicio01.MVC.WEB/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercicio01.MVC.WEB.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+
+        public Cliente() { }
+        public Cliente(int id, string nome, int idade, string telefone, DateTime dtNascimento, string endereco)
+        {
+            this.ClienteID = id;
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Telefone = telefone;
+            this.DataNascimento = dtNascimento;
+            this.Endereco = endereco;
+        }
+    }
+}
